c50e0c5a79a0a9220ae3ba03a9a999ba5a179cdd
vung177 <vu.ng177@gmail.com>
2026-04-07 16:46:35 +0900

    updated exercise ss10

---
+++ Session 10/exercise6/exercise6.js
@@ -0,0 +1,5 @@
+let math = Number(prompt('Enter the math score: '));
+let physics = Number(prompt('Enter the physics score: '));
+let chemistry = Number(prompt('Enter the chemistry score: '));
+let average = (math + physics + chemistry) / 3;
+console.log('The average score is: ' + average);
