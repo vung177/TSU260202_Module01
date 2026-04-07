4dd5eb7834478c18fa99c7f6973d01ed29125fe8
vung177 <vu.ng177@gmail.com>
2026-04-07 16:41:57 +0900

    updated exercise ss10

---
+++ Session 10/exercise3/exercise3.js
@@ -0,0 +1,5 @@
+// Cần kết hợp với Number để đổi kiểu dữ liệu từ chuỗi sang số, nếu không có thể sẽ bị lỗi dữ liệu khi thực hiện phép nhân
+let width = Number(prompt('Enter the width: '));
+let height = Number(prompt('Enter the height: '));
+let area = width * height;
+console.log('The area of the rectangle is: ' + area);
---
+++ Session 10/exercise4/exercise4.js
@@ -0,0 +1,4 @@
+let firstNumber = Number(prompt('Enter the first number: '));
+let secondNumber = Number(prompt('Enter the second number: '));
+let sum = firstNumber + secondNumber;
+alert('The sum of ' + firstNumber + ' and ' + secondNumber + ' is: ' + sum);
