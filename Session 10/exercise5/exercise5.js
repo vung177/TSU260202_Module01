70a556304223f6602cf2746ffc85429d9696fdfb
vung177 <vu.ng177@gmail.com>
2026-04-07 16:43:30 +0900

    updated exercise ss10

--- Session 10/exercise5/exercise5.js
+++ Session 10/exercise5/exercise5.js
@@ -1,4 +1,4 @@
 let moneyUSD = Number(prompt('Enter the amount in USD: '));
 let exRate = 25000;
 let moneyVND = moneyUSD * exRate;
-console.log(moneyUSD + 'USD is equal to ' + moneyVND + ' VND');
+console.log(moneyUSD + ' USD is equal to ' + moneyVND + ' VND');
