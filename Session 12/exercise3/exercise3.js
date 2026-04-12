d8800468a54d111c13befaf04876bb625841f12e
vung177 <vu.ng177@gmail.com>
2026-04-12 20:07:19 +0900

    updated exercise ss12

--- Session 12/exercise3/exercise3.js
+++ Session 12/exercise3/exercise3.js
@@ -2,6 +2,7 @@ let count = 0;
 let numberA = 2;
 
 while (count < 20) {
+  //chữ Prime trong tiếng Anh chuyên ngành là số Nguyên tố
   let isPrime = true;
   for (let i = 2; i <= Math.sqrt(numberA); i++) {
     if (numberA % i === 0) {
